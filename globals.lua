e1ad4927bf3e040f910b5fda2a6d34d88ba72976
Zee1234 <zeeatgit@gmail.com>
2016-08-15 22:05:17 -0500

    Some setup and organization

---
+++ globals.lua
@@ -0,0 +1,13 @@
+g_Plugin = {}
+g_LDH = false
+g_Placeholders = {}
+--[[
+["placeholder_text"] = {
+  ["help_text"] = "text"
+  ["plugin_name"] = "name"
+  ["callback_function"] = "function"
+  ["req_cPlayer"] = true/false
+  ["has_equals"] = true/false
+}
+--]]
+g_Plugins = {}
---
+++ main.lua
@@ -0,0 +1,34 @@
+function Initialize(a_Plugin)
+  a_Plugin:SetName("PlaceholderAPI")
+  a_Plugin:SetVersion(1.0)
+  
+  if not cPluginManager:ForEachPlugin(function(a_plugin)
+      if a_plugin:GetName() == LibDependHandle then return true end
+    end
+  )
+  then
+    LDHSetup()
+  else
+    cPluginManager:AddHook(cPluginManager.HOOK_PLUGINS_LOADED, LDHSetup)
+    cRoot:Get():GetDefaultWorld():ScheduleTask(1,LDHSetup)
+  end
+  
+  g_Plugin = a_Plugin
+	
+  LOG("Initialized API API v." .. a_Plugin:GetVersion())
+  return true
+end
+
+function OnDisable()
+  
+  cPluginManager:CallPlugin("LibDependHandler","UnregisterLibrary","PlaceholderAPI")
+  
+  LOG("PlaceholderHooker is disabled")
+end
+
+function LDHSetup()
+  if not g_LDH then
+    cPluginManager:CallPlugin("LibDependHandler","RegisterLibrary","PlaceholderAPI")
+    g_LDH = true
+  end
+end
