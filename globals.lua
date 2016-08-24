4e5eb565b82b99bafe57195e0f0811f0d1385b32
Zee1234 <zeeatgit@gmail.com>
2016-08-23 20:59:57 -0500

    Define string split function

--- globals.lua
+++ globals.lua
@@ -1,13 +1,14 @@
 g_Plugin = {}
 g_LDH = false
 g_Placeholders = {}
---[[
-["placeholder_text"] = {
-  ["help_text"] = "text"
-  ["plugin_name"] = "name"
-  ["callback_function"] = "function"
-  ["req_cPlayer"] = true/false
-  ["has_equals"] = true/false
-}
---]]
 g_Plugins = {}
+
+cPlaceholder = dofile(cPluginManager:GetCurrentPlugin():GetLocalFolder() .. "/classes/cPlaceholder.lua")
+
+function split(a_string,a_sep)
+  a_sep = a_sep or ":"
+  local fields = {}
+  local pattern = string.format("([^%s]+)", a_sep)
+  a_string:gsub(pattern, function(c) fields[#fields+1] = c end)
+  return fields
+end
